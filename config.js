8b2877487be00b21bc370a518ef56012afad37cd
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-07-14 13:10:41 -0500

    Update config.js

--- config.js
+++ config.js
@@ -11,47 +11,18 @@ global.botnumber = ""
 global.confirmCode = ""
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
-  ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
-  ['5218442286089', '💫 Mystic - Collaborator 4 💫', true],
-  ['50246028932', '💫 Mystic - Collaborator 5 💫', true],
-  ['5212412377467', '💫 Mystic - Collaborator 6 💫', true],
-  ['5215517489568', '💫 Mystic - Rey Endymion 💫', false],
-  ['59895555511', '💫 Mystic - Collaborator 8 💫', true],
-  ['573012482597', '💫 Mystic - OfcDiego', true],
-  ['59175655964', '💫 Mystic - By Alba070503 💫', true],
-  ['393518398856','💫 Mystic - Tester Fabri115💫', true],
-  ['593968585283'],
-  ['5492266466080'],
-  ['5218442114446'],
-  ['59894808483'],
-  ['593980586516'], 
-  ['595975740803'],  
-  ['5492266613038'],
-  ['50497150165'],
-  ['51906662557'],
-  ['573183650526'], 
-  ['5217441298510'], 
-  ['5217294888993'],
-  ['595992611272'],
-  ['5219992843892'],
-  ['573147616444'], 
-  ['5219991402134'],
-  ['5219996125657'],
-  ['5493794297363'], 
-  ['5492215034412']
-];
+  ['593979133620', '👑 Spider - Creador 👑', true],
+  ['50764206082', '💫 Spider - Collaborator 2 💫', true]];
 
-global.suittag = ['5219993404349'];
-global.prems = ['51995386439'];
+global.suittag = ['593979133620'];
+global.prems = ['593979133620'];
 
 global.packname = 'Sticker';
-global.author = 'The Mystic - Bot';
-global.wm = 'The Mystic - Bot';
-global.titulowm = 'The Mystic - Bot';
-global.titulowm2 = `The Mystic - Bot`
-global.igfg = 'The Mystic - Bot';
+global.author = '𝙎𝙋𝙄𝘿𝙀𝙍 𝘽𝙊𝙏';
+global.wm = '𝙎𝙋𝙄𝘿𝙀𝙍 𝘽𝙊𝙏 ;
+global.titulowm = '𝙎𝙋𝙄𝘿𝙀𝙍 𝘽𝙊𝙏';
+global.titulowm2 = `𝙎𝙋𝙄𝘿𝙀𝙍 𝘽𝙊𝙏 `
+global.igfg = '𝙎𝙋𝙄𝘿𝙀𝙍 𝘽𝙊𝙏 ';
 global.wait = '*[ ⏳ ] Cargando...*';
 
 global.imagen1 = fs.readFileSync('./Menu2.jpg');
